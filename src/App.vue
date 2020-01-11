f0cf6291de9dc94b3776b3b0319cd524eb5d93e6
cattdar1900 <towlovelove@gmail.com>
2020-01-11 23:46:21 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>System</span>
+        <span class="font-weight-light"> Analysis and Design</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      
+        
+      
+    </v-app-bar>
+
+    <v-content>
+      <!-- <VideoRental/> -->
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    username: 'Tanapon Kongjaroensk',
+  }),
+};
+</script>
---
+++ src/components/VideoRental.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+   
+  </v-container>
+</template>
+
+<script>
+import http from "../http-common";
+
+export default {
+  name: "videoRental",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+  
+    /* eslint-enable no-console */
+  },
+  mounted() {
+   
+  }
+};
+</script>
